05134d6b65015ad13b62a764b1acdb72218e56a6
rksazid <rksazid@gmail.com>
2019-03-20 15:44:09 +0600

    Complete practise

---
+++ date_time.rb
@@ -0,0 +1,13 @@
+t1  = Time.new
+puts t1.inspect
+
+t1  = Time.now
+puts t1.inspect
+
+puts t1.year
+
+puts Time.local(2008,7,8,9,10)
+
+puts t1.to_a
+
+puts ('rk'..'tm').to_a
---
+++ hashes.rb
@@ -0,0 +1,7 @@
+months = Hash.new ("month")
+
+puts "#{months[10]}"
+
+H = Hash["a"=>100, "b"=>200, [1,"jan"]=> "january"]
+
+puts H["a"]
